e4eaabd2e81f23c057b68245a5a7ce7b3e61f5e8
System Administrator <root@appledeMac-mini.local>
2014-11-21 18:18:36 +0900

    Initial commit

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/joyjoycc/admin/');
+define('HTTP_CATALOG', 'http://localhost/joyjoycc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/joyjoycc/admin/');
+define('HTTPS_CATALOG', 'http://localhost/joyjoycc/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/XAMPP/xamppfiles/htdocs/joyjoycc/admin/');
+define('DIR_SYSTEM', '/Applications/XAMPP/xamppfiles/htdocs/joyjoycc/system/');
+define('DIR_DATABASE', '/Applications/XAMPP/xamppfiles/htdocs/joyjoycc/system/database/');
+define('DIR_LANGUAGE', '/Applications/XAMPP/xamppfiles/htdocs/joyjoycc/admin/language/');
+define('DIR_TEMPLATE', '/Applications/XAMPP/xamppfiles/htdocs/joyjoycc/admin/view/template/');
+define('DIR_CONFIG', '/Applications/XAMPP/xamppfiles/htdocs/joyjoycc/system/config/');
+define('DIR_IMAGE', '/Applications/XAMPP/xamppfiles/htdocs/joyjoycc/image/');
+define('DIR_CACHE', '/Applications/XAMPP/xamppfiles/htdocs/joyjoycc/system/cache/');
+define('DIR_DOWNLOAD', '/Applications/XAMPP/xamppfiles/htdocs/joyjoycc/download/');
+define('DIR_LOGS', '/Applications/XAMPP/xamppfiles/htdocs/joyjoycc/system/logs/');
+define('DIR_CATALOG', '/Applications/XAMPP/xamppfiles/htdocs/joyjoycc/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'joyjoycc_db');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/joyjoycc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/joyjoycc/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/XAMPP/xamppfiles/htdocs/joyjoycc/catalog/');
+define('DIR_SYSTEM', '/Applications/XAMPP/xamppfiles/htdocs/joyjoycc/system/');
+define('DIR_DATABASE', '/Applications/XAMPP/xamppfiles/htdocs/joyjoycc/system/database/');
+define('DIR_LANGUAGE', '/Applications/XAMPP/xamppfiles/htdocs/joyjoycc/catalog/language/');
+define('DIR_TEMPLATE', '/Applications/XAMPP/xamppfiles/htdocs/joyjoycc/catalog/view/theme/');
+define('DIR_CONFIG', '/Applications/XAMPP/xamppfiles/htdocs/joyjoycc/system/config/');
+define('DIR_IMAGE', '/Applications/XAMPP/xamppfiles/htdocs/joyjoycc/image/');
+define('DIR_CACHE', '/Applications/XAMPP/xamppfiles/htdocs/joyjoycc/system/cache/');
+define('DIR_DOWNLOAD', '/Applications/XAMPP/xamppfiles/htdocs/joyjoycc/download/');
+define('DIR_LOGS', '/Applications/XAMPP/xamppfiles/htdocs/joyjoycc/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'joyjoycc_db');
+define('DB_PREFIX', 'oc_');
+?>
